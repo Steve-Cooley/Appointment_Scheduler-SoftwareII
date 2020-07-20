5c44dfad44c1a213263205611ca5ec5ec55003c4
Steve Cooley <scooley138@gmail.com>
2020-07-20 13:37:33 -0700

    First Commit.  Think I have most widgets in place.  Need to start implementing things.

---
+++ src/ViewControllers/LoginController.java
@@ -0,0 +1,5 @@
+package ViewControllers;
+
+public class LoginController  {
+
+}
---
+++ src/ViewControllers/Main.java
@@ -0,0 +1,25 @@
+package ViewControllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+        primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(root, 626, 417));  //This seems to set the size of my window, ignores prefheight/prefwidth
+        primaryStage.setScene(new Scene(root));  //Leaving h,w params unset allows them to be set in fxml by scenbuilder
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
